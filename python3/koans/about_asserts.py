4865cab0b8acb94ed9bd42bd3cca0b4953b91b44
Guinslym <agmond@gmx.com.br>
2016-08-22 16:22:55 -0400

    change order

--- python3/koans/about_asserts.py
+++ python3/koans/about_asserts.py
@@ -16,17 +16,17 @@ class AboutAsserts(Koan):
 
         self.assertTrue(False) # This should be True
 
-    def test_assert_with_message(self):
-        """
-        Enlightenment may be more easily achieved with appropriate messages.
-        """
-        self.assertTrue(False, "This should be True -- Please fix this")
-
     def test_fill_in_values(self):
         """
         Sometimes we will ask you to fill in the values
         """
         self.assertEqual(__, 1 + 1)
+
+    def test_assert_not_equal_values(self):
+        """
+        Sometimes we will ask you to fill in the values
+        """
+        self.assertNotEqual(__, 1 + 1)
 
     def test_assert_equality(self):
         """
@@ -44,35 +44,3 @@ class AboutAsserts(Koan):
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
-
-    def test_that_unittest_asserts_work_the_same_way_as_python_asserts(self):
-        """
-        Understand what lies within.
-        """
-
-        # This throws an AssertionError exception
-        assert False
-
-    def test_that_sometimes_we_need_to_know_the_class_type(self):
-        """
-        What is in a class name?
-        """
-
-        # Sometimes we will ask you what the class type of an object is.
-        #
-        # For example, contemplate the text string "navel". What is its class type?
-        # The koans runner will include this feedback for this koan:
-        #
-        #   AssertionError: '-=> FILL ME IN! <=-' != <type 'str'>
-        #
-        # So "navel".__class__ is equal to <type 'str'>? No not quite. This
-        # is just what it displays. The answer is simply str.
-        #
-        # See for yourself:
-
-        self.assertEqual(__, "navel".__class__) # It's str, not <type 'str'>
-
-        # Need an illustration? More reading can be found here:
-        #
-        #   http://bit.ly/__class__
-
