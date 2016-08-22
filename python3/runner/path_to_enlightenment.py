88463d638502f34b3386963898c697a3f4215c7b
Guinslym <agmond@gmx.com.br>
2016-08-22 16:40:09 -0400

    other

--- python3/runner/path_to_enlightenment.py
+++ python3/runner/path_to_enlightenment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# The path to enlightenment starts with the following:
+# The path to enl ightenment starts with the following:
 
 import unittest
 
