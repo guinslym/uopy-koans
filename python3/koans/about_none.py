ab716066715a418841619626597ed972fe2accb6
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-08-23 01:36:41 -0400

    none

--- python3/koans/about_none.py
+++ python3/koans/about_none.py
@@ -8,14 +8,16 @@
 from runner.koan import *
 
 class AboutNone(Koan):
-
+    '''
+    None is used to represent the absence of a value
+    '''
     def test_none_is_universal(self):
         "There is only one None"
-        self.assertEqual(____, None is None)
+        self.assertEqual(True, None is None)
 
     def test_none_is_distinct(self):
         """
         None is distinct from other things which are False.
         """
-        self.assertEqual(__, None is not 0)
-        self.assertEqual(__, None is not False)
+        self.assertEqual(True, None is not 0)
+        self.assertEqual(True, None is not False)
